e0b0f37c2b41b284d853e065225d8ca475cd8ead
Rob C <robcabardo@gmail.com>
2015-06-03 00:15:15 -0500

    initial commit

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output consoleVar = new ConsoleOutput();
+            //Output fileVar = new FileOutput("C:/Work/Claim/foo.txt");
+
+            Output outputVar;
+            string response = Console.ReadLine();
+            if (response == "h")
+            {
+                outputVar = new ConsoleOutput();
+            }
+            else
+            {
+                outputVar = new FileOutput("C:/Work/Claim/foo.txt");
+            }
+
+            WriteSomeStuffToOutput(outputVar);
+            //WriteSomeStuffToOutput(fieVar);
+
+            Foo(outputVar as ConsoleOutput);
+            //Foo(fileOutput);
+
+            Console.ReadLine();
+        }
+
+        static void WriteSomeStuffToOutput(Output output)
+        {
+            output.EchoSomethingToALine("Howdy");
+            output.EchoSomethingToALine("Goodbye");
+        }
+
+        static void Foo(ConsoleOutput output)
+        {
+            if (output == null)
+            {
+                return;
+            }
+
+            output.EchoSomethingToALine("Needs console output");
+            output.ChangeColor();
+        }
+    }
+
+    abstract class Output
+    {
+        public abstract void EchoSomethingToALine(string value);
+    }
+}
